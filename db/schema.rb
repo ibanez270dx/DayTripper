2f02610af5b69b787e1561a7dbec025bec99e109
Jeff Miller <jeff@humani.se>
2016-01-23 12:29:42 -0800

    Add Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123185801) do
+ActiveRecord::Schema.define(version: 20160123202059) do
 
   create_table "activities", force: :cascade do |t|
-    t.integer  "neighborhood_id"
-    t.integer  "category_id"
-    t.string   "name"
-    t.string   "address"
-    t.string   "location"
-    t.text     "description"
-    t.decimal  "rating"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "neighborhood_id", limit: 4
+    t.integer  "category_id",     limit: 4
+    t.string   "name",            limit: 255
+    t.string   "address",         limit: 255
+    t.string   "location",        limit: 255
+    t.text     "description",     limit: 65535
+    t.decimal  "rating",                        precision: 10
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
