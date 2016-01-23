fbbd85bc2999063bb791884489768b2d46eab5fd
icstunna <mdelostr@gmail.com>
2016-01-23 11:50:56 -0800

    created api calls for yelp

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,13 @@
 class ApiController < ActionController::API
-  def generate
-  end
+  require 'yelp'
 
+  def generate
+    client = Yelp::Client.new({
+      consumer_key: Rails.application.secrets.yelp_consumer_key,
+      consumer_secret: Rails.application.secrets.yelp_consumer_secret,
+      token: Rails.application.secrets.yelp_token,
+      token_secret: Rails.application.secrets.yelp_token_secret})
+
+    p client.search("San Francisco")
+  end
 end
