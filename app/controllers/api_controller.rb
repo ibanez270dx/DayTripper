11644e740b047f38d8a202b85ede2ec0555c4435
icstunna <mdelostr@gmail.com>
2016-01-23 15:39:18 -0800

    returns JSON object of Img url's from Yelp API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,9 +8,13 @@ class ApiController < ActionController::API
       token: Rails.application.secrets.yelp_token,
       token_secret: Rails.application.secrets.yelp_token_secret})
 
-    response = client.search("San Francisco", {term: "Kokkari Estiatorio", location: "200 Jackson St" })
-    x = response.businesses
-    # @business = response.business.name
-    render json: x
+    response = client.search("San Francisco", {term: params[:business_name], location: params[:address] })
+    @business = response.businesses[0]
+
+    render json: {rating_img_url: @business.rating_img_url,
+                  rating_img_url_small:
+                  @business.rating_img_url_small,
+                  image_url: @business.image_url,
+                  snippet_image_url: @business.snippet_image_url}
   end
 end
